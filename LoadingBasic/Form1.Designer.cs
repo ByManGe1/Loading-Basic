f3eacafbc64cbfa2a43e3fbc10119ee61db345b4
ManGe1 <123424785+ByManGe1@users.noreply.github.com>
2023-05-12 01:28:04 +0200

    Add files via upload

---
+++ LoadingBasic/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace LoadingBasic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.picboxPB = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picboxPB)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(266, 164);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(232, 37);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Loading Basic";
+            // 
+            // picboxPB
+            // 
+            this.picboxPB.BackColor = System.Drawing.Color.White;
+            this.picboxPB.Location = new System.Drawing.Point(1, 377);
+            this.picboxPB.Name = "picboxPB";
+            this.picboxPB.Size = new System.Drawing.Size(801, 39);
+            this.picboxPB.TabIndex = 2;
+            this.picboxPB.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(800, 415);
+            this.Controls.Add(this.picboxPB);
+            this.Controls.Add(this.label1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picboxPB)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.PictureBox picboxPB;
+    }
+}
+
---
+++ LoadingBasic/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace LoadingBasic
+{
+    public partial class Form1 : Form
+    {
+        Timer t = new Timer();
+
+        double pbUnit;
+        int pbWIDTH, pbHEIGHT, pbComplete;
+
+        Bitmap bmp;
+        Graphics g;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //get picboxPB dimension
+            pbWIDTH = picboxPB.Width;
+            pbHEIGHT = picboxPB.Height;
+
+            pbUnit = pbWIDTH / 100.0;
+
+            //pbComplete - This is equal to work completed in % [min = 0 max = 100]
+            pbComplete = 0; //0 is the lowest number
+
+            //create bitmap
+            bmp = new Bitmap(pbWIDTH, pbHEIGHT);
+
+            //timer
+            t.Interval = 80;    //in millisecond
+            t.Tick += new EventHandler(this.t_Tick);
+            t.Start(); //the beginning of progress
+        }
+
+        private void t_Tick(object sender, EventArgs e)
+        {
+            //graphics
+            g = Graphics.FromImage(bmp);
+
+            //clear graphics
+            g.Clear(Color.Transparent);
+
+            //draw progressbar
+            g.FillRectangle(Brushes.CornflowerBlue, new Rectangle(0, 0, (int)(pbComplete * pbUnit), pbHEIGHT)); //Progress bar color
+
+            g.DrawString("Loading Assets...", new Font("Bahnschrift", pbHEIGHT / 3), Brushes.Black, new PointF(pbWIDTH / 20 - pbHEIGHT, pbHEIGHT / 5)); //Progress bar text
+
+            //load bitmap in picturebox picboxPB
+            picboxPB.Image = bmp;
+
+            //To keep things simple I am adding +1 to pbComplete every 50ms
+            pbComplete++;
+            if (pbComplete > 100)
+            {
+                g.Dispose();
+                t.Stop();
+               // If you want the Form to change when completing the upload, you must place it in this part
+            }
+        }
+    }
+}
